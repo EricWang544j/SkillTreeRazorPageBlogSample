2be433a597d54c1e0b35f3f2842693dcf623a198
王群鈞 <544j544j@gmail.com>
2020-05-29 11:29:51 +0800

    實作 tag cloud 二種view Components/Tag Helper

---
+++ Helper/Tag/TagCloudTTHelper.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SkillTreeRazorPageBlogSample.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTreeRazorPageBlogSample.Helper.Tag
+{
+    public class TagCloudTTHelper : TagHelper
+    {
+
+
+
+        private readonly RazorPageBlogContext _context;
+        private readonly IUrlHelperFactory UrlHelperFactory;
+        private readonly IActionContextAccessor Accessor;
+
+
+
+
+        public TagCloudTTHelper(
+            RazorPageBlogContext context,
+            IUrlHelperFactory urlHelperFactory, 
+            IActionContextAccessor accessor)
+        {
+            _context = context;
+            UrlHelperFactory = urlHelperFactory;
+            Accessor = accessor;
+        }
+
+       
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var actionContext = Accessor.ActionContext;
+            var urlHelper = UrlHelperFactory.GetUrlHelper(actionContext);
+
+
+            var data = _context.TagCloud.ToDictionary(d => d.Name, d => d.Amount);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<div class='card my-4'>");
+            sb.AppendLine("<h5 class='card-header'>Tag Cloud</h5>");
+            sb.AppendLine("<div class='card-body'>");
+            sb.AppendLine("<ul class='list-unstyled mb-0'>");
+
+            foreach(var item in data)
+            {
+
+                string url = urlHelper.Page("index", "tag", new { tag = item.Key });
+
+                //var allDatakk = new Dictionary<string, string> { { "tag", item.Key }, { "p", "1" } };
+                sb.AppendLine("<li class='d-inline'>");
+                sb.AppendLine($"<a href='{url}'>{item.Key}({item.Value})</a>");
+                sb.AppendLine("</li >");
+            }
+
+            sb.AppendLine("</ul>");
+            sb.AppendLine("</div>");
+            sb.AppendLine("</div>");
+
+
+            output.Content.SetHtmlContent(sb.ToString() );
+        }
+
+    }
+}
---
+++ ViewComponents/TagCloudViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+using SkillTreeRazorPageBlogSample.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillTreeRazorPageBlogSample.ViewComponents
+{
+    public class TagCloudViewComponent : ViewComponent
+    {
+        private readonly RazorPageBlogContext _context;
+       
+
+
+        public TagCloudViewComponent(RazorPageBlogContext context)
+        {
+            _context = context;
+           
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            ViewData.Model = _context.TagCloud.ToDictionary(d => d.Name, d => d.Amount);
+            return View();
+        }
+    }
+
+
+}
